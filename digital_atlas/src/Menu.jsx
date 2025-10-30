ed48f51c1ca8eb13fc681630be7f0bbf65953a33
Robert Eusanio <robeusanio11@gmail.com>
2025-10-30 13:25:06 -0700

    button click functionality WIP

--- digital_atlas/src/Menu.jsx
+++ digital_atlas/src/Menu.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react'
 import './Menu.css'
 
 function Menu () {
+    const [menuActive, setMenuActive] = useState(false)
+    const [menuButtonText, setMenuButtonText] = useState('☰')
+    
+    const toggleMenu = () => setMenuActive(!menuActive)
+    
+    const toggleMenuButton = () => {
+        if (menuActive) {
+            setMenuButtonText('-->')
+        } else {
+            setMenuButtonText('☰')
+        }
+
+    }
+    
+    const handleButtonClick = () => {toggleMenu(); toggleMenuButton();}
     return (
-        <>
-            <div className="menu_container">
-                <h2>Here is the menu.</h2>
+        <>  <div className="floating-menu">
+                <button className="menu-button" onClick={handleButtonClick}>{menuButtonText}</button>
+                <div className={menuActive ? "menu-content-active" : 'menu-content-inactive'}>
+                    <h2>Here is the menu.</h2>
+                    <div>Click something to learn more.</div>
+                </div>
             </div>
         </>
     )
