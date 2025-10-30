73248dd0a0a58ddb5991d96681a17ad34cc76670
Robert Eusanio <robeusanio11@gmail.com>
2025-10-30 13:56:53 -0700

    added use effect for menu button handling

--- digital_atlas/src/Menu.jsx
+++ digital_atlas/src/Menu.jsx
@@ -1,25 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Menu.css'
+import { use } from 'react'
 
 function Menu () {
     const [menuActive, setMenuActive] = useState(false)
     const [menuButtonText, setMenuButtonText] = useState('☰')
     
-    const toggleMenu = () => setMenuActive(!menuActive)
+    const toggleMenu = () => setMenuActive(!menuActive, () => { this.toggleMenuButton() })
     
-    const toggleMenuButton = () => {
-        if (menuActive) {
-            setMenuButtonText('-->')
-        } else {
-            setMenuButtonText('☰')
-        }
+    useEffect(() => {
+        toggleMenuButton();
+    }, [menuActive])
 
+    const toggleMenuButton = () => {
+        setMenuButtonText(menuActive ? '-->' : '☰')
     }
     
-    const handleButtonClick = () => {toggleMenu(); toggleMenuButton();}
+    // const handleButtonClick = () => {
+    //     toggleMenu(); 
+    //     toggleMenuButton();
+    // }
+
     return (
         <>  <div className="floating-menu">
-                <button className="menu-button" onClick={handleButtonClick}>{menuButtonText}</button>
+                <button className="menu-button" onClick={toggleMenu}>{menuButtonText}</button>
                 <div className={menuActive ? "menu-content-active" : 'menu-content-inactive'}>
                     <h2>Here is the menu.</h2>
                     <div>Click something to learn more.</div>
