790d6230ad15aa0e2c62f94279421b9adf3d5166
robeusanio11 <robeusanio11@gmail.com>
2025-10-30 10:12:58 -0700

    changed center

--- digital_atlas/src/App.jsx
+++ digital_atlas/src/App.jsx
@@ -1,21 +1,66 @@
 import { useState } from 'react'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Polyline, Polygon } from 'react-leaflet'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const center = [30, 40]
+
+  const polyline = [
+    [51.505, -0.09],
+    [51.51, -0.1],
+    [51.51, -0.12],
+  ]
+
+  const multiPolyline = [
+    [
+      [51.5, -0.1],
+      [51.5, -0.12],
+      [51.52, -0.12],
+    ],
+    [
+      [51.5, -0.05],
+      [51.5, -0.06],
+      [51.52, -0.06],
+    ],
+  ]
+
+  const polygon = [
+    [51.515, -0.09],
+    [51.52, -0.1],
+    [51.52, -0.12],
+  ]
+
+  const multiPolygon = [
+    [
+      [51.51, -0.12],
+      [51.51, -0.13],
+      [51.53, -0.13],
+    ],
+    [
+      [51.51, -0.05],
+      [51.51, -0.07],
+      [51.53, -0.07],
+    ],
+  ]
+
+  const limeOptions = { color: 'lime' }
+  const purpleOptions = { color: 'purple' }
 
   return (
     <>
-      {/* <div class="mapContainer"> */}
-        <MapContainer className="mapContainer" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer className="mapContainer" center={center} zoom={5} scrollWheelZoom={false}>
           <TileLayer 
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+
+          <Polyline pathOptions={limeOptions} positions={polyline} />
+          <Polyline pathOptions={limeOptions} positions={multiPolyline} />
+          <Polygon pathOptions={purpleOptions} positions={polygon} />
+          <Polygon pathOptions={purpleOptions} positions={multiPolygon} />
         </MapContainer>
-        <div id="map"></div>
-      {/* </div> */}
+        
     </>
   )
 }
