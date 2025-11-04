0286abf49fcb136d27c15c1c0413152f63ab3e20
Robert Eusanio <robeusanio11@gmail.com>
2025-11-04 14:49:09 -0800

    messing around with geoJSON

--- digital_atlas/src/App.jsx
+++ digital_atlas/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
-import { MapContainer, TileLayer, useMap, Polyline, Polygon } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Polyline, Polygon, GeoJSON } from 'react-leaflet'
 import './App.css'
 import Menu from './Menu.jsx'
 import TimeSlider from './TimeSlider.jsx'
 import HelpModal from './HelpModal.jsx'
+import '../public/geoJSONstates.json'
+import fs from 'fs';
 
 function App() {
   const center = [30, 40]
@@ -56,7 +58,7 @@ function App() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
+      
         <Polyline pathOptions={limeOptions} positions={polyline} />
         <Polyline pathOptions={limeOptions} positions={multiPolyline} />
         <Polygon pathOptions={purpleOptions} positions={polygon} />
