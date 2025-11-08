0e7763f5b5c308b5fd42ce6e640d83b9d613224e
robeusanio11 <robeusanio11@gmail.com>
2025-11-08 10:18:46 -0800

    deleted test polygons add json file

--- digital_atlas/src/App.jsx
+++ digital_atlas/src/App.jsx
@@ -8,47 +8,6 @@ import HelpModal from './HelpModal.jsx'
 function App() {
   const center = [30, 40]
 
-  const polyline = [
-    [51.505, -0.09],
-    [51.51, -0.1],
-    [51.51, -0.12],
-  ]
-
-  const multiPolyline = [
-    [
-      [51.5, -0.1],
-      [51.5, -0.12],
-      [51.52, -0.12],
-    ],
-    [
-      [51.5, -0.05],
-      [51.5, -0.06],
-      [51.52, -0.06],
-    ],
-  ]
-
-  const polygon = [
-    [51.515, -0.09],
-    [51.52, -0.1],
-    [51.52, -0.12],
-  ]
-
-  const multiPolygon = [
-    [
-      [51.51, -0.12],
-      [51.51, -0.13],
-      [51.53, -0.13],
-    ],
-    [
-      [51.51, -0.05],
-      [51.51, -0.07],
-      [51.53, -0.07],
-    ],
-  ]
-
-  const limeOptions = { color: 'lime' }
-  const purpleOptions = { color: 'purple' }
-
   return (
     <>
       <MapContainer className="mapContainer" center={center} zoom={5} scrollWheelZoom={false}>
@@ -56,11 +15,6 @@ function App() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
-        <Polyline pathOptions={limeOptions} positions={polyline} />
-        <Polyline pathOptions={limeOptions} positions={multiPolyline} />
-        <Polygon pathOptions={purpleOptions} positions={polygon} />
-        <Polygon pathOptions={purpleOptions} positions={multiPolygon} />
       </MapContainer>
       <Menu />
       <TimeSlider />
