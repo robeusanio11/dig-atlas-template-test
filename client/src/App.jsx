b31a0b03e746e181c4626352f06a52128e2dc7dd
robeusanio11 <robeusanio11@gmail.com>
2025-11-08 11:05:18 -0800

    remove unused components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router'
 import { MapContainer, TileLayer, useMap, Polyline, Polygon, GeoJSON } from 'react-leaflet'
 import './App.css'
 import Menu from './Menu.jsx'
 import TimeSlider from './TimeSlider.jsx'
-// import HelpModal from './HelpModal.jsx'
 import geoJSONstates from './assets/geoJSONstates.json'
 import geoJSONcountries from './assets/custom.geo.json'
-import { useNavigate } from 'react-router'
-import fs from 'fs';
 
 function App() {
   const center = [30, 40]
   const navigate = useNavigate();
-  // geoJSONcountries.GeoJSON
   
   return (
     <>
@@ -28,7 +25,6 @@ function App() {
       <Menu />
       <TimeSlider />
       <button className='home-button' onClick={() => navigate("/")}>Home</button>
-      {/* <HelpModal /> */}
     </>
   )
 }
